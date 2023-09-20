1872488434f539be37efc1448b62d13e317a8895
waicharikim <waicharikimani@gmail.com>
2023-09-21 00:14:03 +0300

    main.c update

--- main.c
+++ main.c
@@ -7,13 +7,28 @@
  *
  * return: 0 for success
  */
+void arg_check(int);
 
-int main(int argc, char **argv)
-	(void) argv;
-  if (argc != 2)
-    {
-      fprintf(stderr, "USAGE: monty file\n");
-      exit(EXIT_FAILURE);
-    }
-  return 0;
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	arg_check(argc);
+	file_check(argv[1]);
+	return 0;
+}
+
+void arg_check(int argc)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+char *file_check(char *file_name)
+{
+	file_arg = malloc(sizeof(file_t));
+	if (file_arg == NULL)
+		malloc_fail();
+	file_arg
 }
