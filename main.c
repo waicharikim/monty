e64c598e4f73e5f2f5bf7e5cbe2e45713854045a
waicharikim <waicharikimani@gmail.com>
2023-09-20 18:00:10 +0300

    check filename

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
-/.
+#include "monty.h"
+
+/** 
+ * main - main program entry
+ * @argc: argument count
+ * @argv: argument string
+ *
+ * return: 0 for success
+ */
+
+main(int argc, char **argv)
+{
+  if (argc != 2)
+    {
+    fputs("USAGE: monty file", stderr);
+    exit(EXIT_FAILURE);
+    }
+  return 0;
+}
