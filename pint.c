64209384d32b00ee3f6add3120d60bf7e03b9f4e
JAmwenya <jnehondo@gmail.com>
2023-09-21 20:23:31 +0000

    deletion

---
+++ pint.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * pint - prints value at top of a stack & exits if stack is empty
+ * @stack: double pointer to the head of the stack
+ * @line_number: 
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	**stack = *head
