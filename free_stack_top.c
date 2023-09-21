0bed24e04c157f20b0f7575245d115bef8206867
waicharikim <waicharikimani@gmail.com>
2023-09-22 02:05:23 +0300

    minor typos

---
+++ free_stack_top.c
@@ -0,0 +1,13 @@
+/**
+ * free_stack_top - Frees the memory allocated for the stack_top
+ *
+ * Return: NULL;
+ */
+
+void free_stack_top(void)
+{
+	if (file_arg->stack_top)
+		free_stack(file_arg->stack_top);
+
+	file_arg->stack_top = NULL;
+}
