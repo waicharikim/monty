f67fb5cff332cf9c120f2960b502165fe5d2585c
waicharikim <waicharikimani@gmail.com>
2023-09-22 05:59:37 +0300

    bug fix

---
+++ free_tokens.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * free_tokens - frees tokens
+ *
+ * Return: void
+ */
+void free_tokens(void)
+{
+	int i = 0;
+	if (file_arg->tokens == NULL)
+		return;
+	while (file_arg->tokens[i])
+	{
+		free(file_arg->tokens[i]);
+		i++;
+	}
+	free(file_arg->tokens);
+	file_arg->tokens = NULL;
+}
